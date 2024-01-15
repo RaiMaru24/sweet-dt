13ce19e1d385feb275241813c48a709e0ed5f7c7
Niranjan BR <niranjankannan2003@gmail.com>
2024-01-15 16:52:00 +0000

    sweet: Update blobs from MIUI V14.0.9.0.TKFEUXM Signed-off-by: Niranjan BR <niranjankannan2003@gmail.com>

--- apollo_sweet.mk
+++ apollo_sweet.mk
@@ -25,6 +25,6 @@ PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 TARGET_USES_AOSP_RECOVERY := true
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="sweet_eea-user 13 RKQ1.210614.002 V14.0.6.0.TKFEUXM release-keys"
+    PRIVATE_BUILD_DESC="sweet_eea-user 13 RKQ1.210614.002 V14.0.9.0.TKFEUXM release-keys"
 
-BUILD_FINGERPRINT := Redmi/sweet_eea/sweet:13/RKQ1.210614.002/V14.0.6.0.TKFEUXM:user/release-keys
+BUILD_FINGERPRINT := Redmi/sweet_eea/sweet:13/RKQ1.210614.002/V14.0.9.0.TKFEUXM:user/release-keys
